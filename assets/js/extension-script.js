f9118a990a909b7e60a3a4596fb14f91885613cd
Andrea Tombolato <andreacw96@gmail.com>
2025-03-18 12:55:44 +0100

    copy other scripts

---
+++ assets/js/extension-script.js
@@ -0,0 +1,162 @@
+function toggleAccordion(id) {
+    const content = document.getElementById(id);
+    if (!content) return;
+    const header = content.previousElementSibling;
+    if (!header || !header.classList.contains('accordion-header')) return;
+    const icon = header.querySelector('.accordion-icon');
+    if (!icon) return;
+    const isOpen = content.classList.contains('open');
+    if (!isOpen) {
+        content.classList.add('open');
+        icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
+        const baseId = id.replace('-acc', '');
+        history.pushState(null, null, `#${baseId}`);
+        setTimeout(() => {
+            const offset = 250;
+            const elementPosition = header.getBoundingClientRect().top + window.scrollY;
+            window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
+        }, 100);
+    } else {
+        content.classList.remove('open');
+        icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
+        history.pushState(null, null, window.location.pathname);
+    }
+}
+
+
+
+// ====================================================================================================================
+function copyToClipboard(id) {
+    const copyText = document.getElementById(id);
+    const button = copyText.nextElementSibling;
+    navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
+        button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+        button.classList.add('copied');
+        setTimeout(() => {
+            button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+            button.classList.remove('copied');
+        }, 2500);
+    });
+}
+
+// ====================================================================================================================
+function copyURLToClipboard(url) {
+    const button = document.querySelector('#browser-source-url-acc .copy-btn');
+    navigator.clipboard.writeText(url).then(() => {
+        button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+        button.classList.add('copied');
+        setTimeout(() => {
+            button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+            button.classList.remove('copied');
+        }, 2500);
+    });
+}
+
+// ====================================================================================================================
+function loadImportString(file) {
+    const importText = document.getElementById("import-string");
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            importText.textContent = data.substring(0, 200) + '...';
+            importText.setAttribute("data-copy-text", data);
+        })
+        .catch(err => console.error("Error loading file:", err));
+}
+
+// ====================================================================================================================
+document.addEventListener("DOMContentLoaded", function () {
+    const hash = window.location.hash.substring(1);
+    if (hash) {
+        const accId = `${hash}-acc`;
+        toggleAccordion(accId);
+        setTimeout(() => {
+            const header = document.getElementById(accId)?.previousElementSibling;
+            if (header) {
+                const offset = 250;
+                const elementPosition = header.getBoundingClientRect().top + window.scrollY;
+                window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
+            }
+        }, 200);
+    }
+    const currentPage = window.location.pathname;
+    if (currentPage.includes("dynamic-timers")) {
+        loadImportString("/action-imports/dynamic-timers.txt");
+    } else if (currentPage.includes("rotator")) {
+        loadImportString("/action-imports/rotator.txt");
+    } else if (currentPage.includes("stream-receipt")) {
+        loadImportString("/action-imports/stream-receipt.txt");
+    } else if (currentPage.includes("spotify-and-sb")) {
+        loadImportString("/action-imports/spotify-and-sb.txt");
+    } else if (currentPage.includes("bluesky-and-sb")) {
+        loadImportString("/action-imports/bluesky-and-sb.txt");
+    } else if (currentPage.includes("user-birthdays")) {
+        loadImportString("/action-imports/user-birthdays.txt");
+    } else if (currentPage.includes("movie-and-tv-show-quiz")) {
+        loadImportString("/action-imports/movie-and-tv-show-quiz.txt");
+    } else if (currentPage.includes("temporary-vip")) {
+        loadImportString("/action-imports/temporary-vip.txt");
+    } else if (currentPage.includes("channel-point-auction")) {
+        loadImportString("/action-imports/channel-point-auction.txt");
+    } else if (currentPage.includes("command-checker")) {
+        loadImportString("/action-imports/command-checker.txt");
+    } else if (currentPage.includes("hugs")) {
+        loadImportString("/action-imports/hugs.txt");
+    } else if (currentPage.includes("live-trigger")) {
+        loadImportString("/action-imports/live-trigger.txt");
+    } else if (currentPage.includes("lurks")) {
+        loadImportString("/action-imports/lurks.txt");
+    } else if (currentPage.includes("mod-tools")) {
+        loadImportString("/action-imports/mod-tools.txt");
+    } else if (currentPage.includes("raffle")) {
+        loadImportString("/action-imports/raffle.txt");
+    } else if (currentPage.includes("random-source-position")) {
+        loadImportString("/action-imports/random-source-position.txt");
+    } else if (currentPage.includes("reward-discount")) {
+        loadImportString("/action-imports/reward-discount.txt");
+    } else if (currentPage.includes("time-trigger")) {
+        loadImportString("/action-imports/time-trigger.txt");
+    } else if (currentPage.includes("tts-queue")) {
+        loadImportString("/action-imports/tts-queue.txt");
+    } else if (currentPage.includes("user-inventory")) {
+        loadImportString("/action-imports/user-inventory.txt");
+    } else if (currentPage.includes("hot-potato")) {
+        loadImportString("/action-imports/hot-potato.txt");
+    } else if (currentPage.includes("quick-maths")) {
+        loadImportString("/action-imports/quick-maths.txt");
+    } else if (currentPage.includes("stardew-valley")) {
+        loadImportString("/action-imports/stardew-valley.txt");
+    }
+});
+
+// ====================================================================================================================
+
+document.addEventListener("DOMContentLoaded", function() {
+  const shareElements = document.querySelectorAll('.share-element');
+  
+  shareElements.forEach(shareEl => {
+    shareEl.addEventListener('click', async function() {
+      const textToCopy = shareEl.getAttribute('data-clipboard-text');
+      
+      try {
+        await navigator.clipboard.writeText(textToCopy);
+        
+        const icon = shareEl.querySelector('.share-icon');
+        const textEl = shareEl.querySelector('.share-text');
+        const originalIcon = icon.getAttribute('data-icon');
+        const originalText = textEl.textContent;
+        
+        icon.setAttribute('data-icon', 'material-symbols:check-rounded');
+        textEl.textContent = 'Copied';
+        
+        setTimeout(function() {
+          icon.setAttribute('data-icon', originalIcon);
+          textEl.textContent = originalText;
+        }, 2500);
+      } catch (err) {
+        console.error('Fehler beim Kopieren: ', err);
+      }
+    });
+  });
+});
+
---
+++ assets/js/script.js
@@ -0,0 +1,196 @@
+// ====================================================================================================================
+const wordToEmojiMap = {
+    "5Head": "/assets/emotes/5head.webp",
+    "Pog": "/assets/emotes/pog.webp",
+    "FeelsBadMan": "/assets/emotes/feelsbadman.webp",
+    "Kapp": "/assets/emotes/kapp.webp",
+    "pepeSmoke": "/assets/emotes/pepesmoke.webp",
+    "MrDestructoid": "/assets/emotes/mrdestructoid.webp",
+    "widetime": "/assets/emotes/widetime.webp",
+    "modCheck": "/assets/emotes/modcheck.webp",
+    "PauseChamp": "/assets/emotes/pausechamp.webp",
+    "Clap": "/assets/emotes/clap.webp",
+    "itsTimeToStop": "/assets/emotes/joji-clock.webp",
+    "rupertRotate": "/assets/emotes/rupert.webp",
+    "kingGABEN": "/assets/emotes/kingGABEN.webp",
+    "thisIsFine": "/assets/emotes/this-is-fine.webp",
+    "SNIFFA": "/assets/emotes/sniffa.webp",
+    "moneyWave": "/assets/emotes/moneyWave.webp",
+    "griffinTyping": "/assets/emotes/peter-griffin-typing.webp",
+    "catJAM": "/assets/emotes/cat-jam.webp",
+    "_EDM": "/assets/emotes/edm.webp",
+    "crunch": "/assets/emotes/crunch.webp",
+    "happie": "/assets/emotes/happie.webp",
+    "CLASSIC": "/assets/emotes/classic.webp",
+    "helloThere": "/assets/emotes/hellothere.webp",
+    "streamer_BOT": "/assets/emotes/streamerbot.webp",
+    "LUL": "/assets/emotes/lul.webp",
+    "catBirthday": "/assets/emotes/cat-birthday.webp",
+    "rightThere": "/assets/emotes/right-there.webp",
+    "PETTHEVIP": "/assets/emotes/pet-the-vip.webp",
+    "catBlush": "/assets/emotes/cat-blush.webp",
+    "clockUwu": "/assets/emotes/clock-uwu.webp",
+    "peepoLive": "/assets/emotes/peepo-live.webp",
+    "RIGGED": "/assets/emotes/rigged.webp"
+};
+
+function replacePlaceholdersWithEmojis(text) {
+    for (const [word, emojiURL] of Object.entries(wordToEmojiMap)) {
+        const emojiTag = `<img src="${emojiURL}" alt="${word}" class="emoji" data-fullsize="${emojiURL}" style="height: 1em; vertical-align: middle;">`;
+
+        text = text.replace(new RegExp(`:\\b${word}\\b:`, 'g'), emojiTag);
+        text = text.replace(new RegExp(`\\b${word}\\b`, 'g'), emojiTag);
+    }
+    return text;
+}
+
+function replaceWordsWithEmojis() {
+    const elements = document.querySelectorAll('p, h1, h2, h3, span, div');
+
+    elements.forEach(element => {
+        let text = element.innerHTML;
+
+        for (const [word, emojiURL] of Object.entries(wordToEmojiMap)) {
+            const emojiTag = `<img src="${emojiURL}" alt="${word}" class="emoji" data-fullsize="${emojiURL}" style="height: 1em; vertical-align: middle;">`;
+            text = text.replace(new RegExp(`\\b${word}\\b`, 'g'), emojiTag);
+        }
+
+        element.innerHTML = text;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    replaceWordsWithEmojis();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const emojis = document.querySelectorAll('.emoji');
+
+    emojis.forEach(emoji => {
+        emoji.addEventListener('mouseenter', function () {
+            const fullsizePreview = document.createElement('div');
+            fullsizePreview.classList.add('emoji-preview');
+            fullsizePreview.style.position = 'absolute';
+            fullsizePreview.style.zIndex = '1000';
+            fullsizePreview.style.top = `${emoji.getBoundingClientRect().top + window.scrollY}px`;
+            fullsizePreview.style.left = `${emoji.getBoundingClientRect().right + 10}px`;
+            fullsizePreview.innerHTML = `<img src="${emoji.getAttribute('data-fullsize')}" style="max-width: 200px; max-height: 200px;">`;
+
+            document.body.appendChild(fullsizePreview);
+
+            emoji.addEventListener('mouseleave', function () {
+                fullsizePreview.remove();
+            });
+        });
+    });
+});
+
+// ====================================================================================================================
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var codeBlocks = document.querySelectorAll('.extension-code-block');
+
+    codeBlocks.forEach(function (block) {
+        var language = block.getAttribute('data-language') || 'none';
+        var pre = document.createElement('pre');
+        var code = document.createElement('code');
+        code.className = 'language-' + language;
+
+        var codeText = block.innerHTML.trim();
+        code.textContent = codeText;
+        pre.appendChild(code);
+        block.parentNode.replaceChild(pre, block);
+
+        if (typeof Prism !== 'undefined') {
+            Prism.highlightElement(code);
+        }
+    });
+});
+
+// ====================================================================================================================
+
+$(document).ready(function(){
+  $('.carousel-1').slick({
+    autoplay: true,
+    autoplaySpeed: 8000,
+    fade: true,
+    speed: 1500,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev" aria-label="Previous">&#10094;</button>',
+    nextArrow: '<button type="button" class="slick-next" aria-label="Next">&#10095;</button>'
+  });
+});
+    // =================================
+$(document).ready(function(){
+  $('.carousel-2').slick({
+    autoplay: true,
+    autoplaySpeed: 12000,
+    speed: 700,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev" aria-label="Previous">&#10094;</button>',
+    nextArrow: '<button type="button" class="slick-next" aria-label="Next">&#10095;</button>'
+  });
+});
+
+// ====================================================================================================================
+
+document.addEventListener("DOMContentLoaded", function() {
+    tippy('.discord-link', {
+        content: 'Need help with something?',
+        placement: 'bottom',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+
+    tippy('.bluesky-link', {
+        content: 'Follow me on Bluesky (or not)',
+        placement: 'bottom',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+
+    tippy('.twitch-link', {
+        content: 'I don\'t stream KEKW',
+        placement: 'bottom',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+
+    tippy('.youtube-link', {
+        content: 'My YouTube channel (german only)',
+        placement: 'bottom',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+
+    tippy('.kofi-button', {
+        content: 'Buy me an Iced Pumpkin Spice Latte',
+        placement: 'bottom',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+    tippy('.product-card', {
+        placement: 'bottom-end',
+        animation: 'scale-subtle',
+        theme: 'light-border',
+        delay: [100, 50], 
+    });
+    
+});
+
+
